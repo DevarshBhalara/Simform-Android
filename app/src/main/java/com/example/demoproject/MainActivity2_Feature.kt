70e08cbfbb35fa0dd2b5c0de7409fd8bc2e956c0
DevarshBhalara <devarshbhalara3072>
2023-02-20 18:07:43 +0530

    Feature 2 XML added

---
+++ app/src/main/java/com/example/demoproject/MainActivity2_Feature.kt
@@ -0,0 +1,11 @@
+package com.example.demoproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2_Feature : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activity2_feature)
+    }
+}
