679b3f035bbf301929059f535d2f35109e726e18
DevarshBhalara <devarshbhalara3072>
2023-02-20 17:43:05 +0530

    Develop branch modified

--- app/src/main/java/com/example/demoproject/MainActivity.kt
+++ app/src/main/java/com/example/demoproject/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.e("Feature 2","Feature 2 is added")
+
     }
 }
