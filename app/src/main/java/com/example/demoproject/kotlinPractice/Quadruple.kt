1e8c74e0675311d0ef47dd5a143393c23001ef93
DevarshBhalara <devarshbhalara3072>
2023-03-21 18:44:02 +0530

    TE7-T686 (21/03/2023) Generics, Collection type , Quadraple

--- app/src/main/java/com/example/demoproject/kotlinPractice/Quadruple.kt
+++ app/src/main/java/com/example/demoproject/kotlinPractice/Quadruple.kt
@@ -7,3 +7,8 @@ data class Quadruple<out A, out B, out C, out D> (val first: A, val second: B, v
     override fun toString() = "($first, $second, $third, $fourth)"
 }
 fun <T> Quadruple<T, T, T, T>.toList() = listOf(first, second, third, fourth)
+
+fun main() {
+    var a = Quadruple(10, 20, 30, 40)
+    println(a.fourth)
+}
