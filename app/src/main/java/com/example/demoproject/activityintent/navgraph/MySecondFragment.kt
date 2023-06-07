3d775c05558110d8d2a44d40d94cf3278318ecea
DevarshBhalara <devarshbhalara3072>
2023-06-07 16:55:03 +0530

    TE7-T1046: NavGraph with ActionBar

--- app/src/main/java/com/example/demoproject/activityintent/navgraph/MySecondFragment.kt
+++ app/src/main/java/com/example/demoproject/activityintent/navgraph/MySecondFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.demoproject.R
+import com.example.demoproject.databinding.FragmentMySecondBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +23,7 @@ class MySecondFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var binding: FragmentMySecondBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +38,17 @@ class MySecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_second, container, false)
+        binding = FragmentMySecondBinding.inflate(layoutInflater)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnGotoThird.setOnClickListener {
+            findNavController().navigate(R.id.second_to_third)
+        }
     }
 
     companion object {
